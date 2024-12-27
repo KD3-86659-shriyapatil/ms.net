dbf1de9f35b739938b84f3b4793c833bc70598e2
Shriya patil <pshriya841@gmail.com>
2024-12-27 07:17:59 +0530

    Assignmnent01

---
+++ Assignment01/Assign1Que2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign1Que2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Enter  numbers1-");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter  numbers2-");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select Opeation (+,-,*,/)-");
+            char operation = Console.ReadKey().KeyChar;
+           
+
+            double result;
+
+            switch (operation) {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine($"result-{num1}+{num2}={result}");
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine($"result-{num1}-{num2}={result}");
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine($"result-{num1}*{num2}={result}");
+                    break;
+                case '/':
+                    result = num1 + num2;
+                    Console.WriteLine($"result-{num1}/{num2}={result}");
+                    break;
+                   
+
+
+            }
+    
+
+
+        }
+    }
+}
---
+++ Assignment01/Assign1Que3/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Assign1Que3
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter  numbers1-");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter  numbers2-");
+            int num2 = int.Parse(Console.ReadLine());
+
+            bool exit = false;
+            while (!exit) {
+                Console.Write("0.Exit");
+                Console.WriteLine("1.Addition");
+                Console.WriteLine("2.Substraction");
+                Console.WriteLine("3.Multiplicatiion");
+                Console.WriteLine("4.Division");
+                Console.WriteLine("Enter your choice-");
+                String choice = Console.ReadLine();
+
+                double result;
+
+                switch (choice) {
+                    case "1":
+                        result = num1 + num2;
+                        Console.WriteLine($"result-{num1}+{num2}={result}");
+                        break;
+                    case "2":
+                        result = num1 - num2;
+                        Console.WriteLine($"result-{num1}-{num2}={result}");
+                        break;
+                    case "3":
+                        result = num1 * num2;
+                        Console.WriteLine($"result-{num1}*{num2}={result}");
+                        break;
+                    case "4":
+                        result = num1 + num2;
+                        Console.WriteLine($"result-{num1}/{num2}={result}");
+                        break;
+                }
+
+            }
+        
+        }
+    }
+}
